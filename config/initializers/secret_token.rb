59e26db35057cf0bc927d9df83ed0beacb7d0497
digitalz4k <leodsirs@gmail.com>
2013-08-27 14:15:49 +0200

    Inicial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Digitalz::Application.config.secret_token = 'f2a92306cfca477336d97d1014078d4a2d2dd471bb304400ad0afb5eff01fc3d1d3826832114bb30b65d31dcce3b659cc36722b37c3f83fa62a6eed9ab9a2835'
